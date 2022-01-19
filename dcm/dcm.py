ed19f30826c97e38c0e2540788f3b0de1c06549d
timo.c <timo.c@kakaoenterprise.com>
2022-01-19 23:41:01 +0900

    1920 pixel from test dcm file

--- dcm/dcm.py
+++ dcm/dcm.py
@@ -15,6 +15,9 @@ def save(src, out):
     img = img.astype(np.uint8)
 
     img_file = os.path.join(out, os.path.basename(src).replace('.dcm', '.png'))
+    # resize
+    # img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+    img = cv2.resize(img, (0, 0), fx=0.9545, fy=0.9545, interpolation = cv2.INTER_NEAREST)
     cv2.imwrite(img_file, img)
 
 
